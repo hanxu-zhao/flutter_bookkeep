169c3034d59d551f18fb92ac301a944327cf6757
zhaohx <hanxu.zhao@ipanda.co.jp>
2020-06-29 14:03:02 +0800

    Create new project

---
+++ lib/ui/home/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// @ProjectName:    flutter_bookkeep
+/// @Package:
+/// @ClassName:      homePage
+/// @Author:         hanxu.zhao
+/// @CreateDate:     2020/6/29 1:57 PM
+/// @Description:
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
